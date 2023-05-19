6a7e8852dc047ceec765bbe059b226305012aa0a
siraz015 <siraj015213@gmail.com>
2023-05-20 02:32:29 +0600

    find spcific toy by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -43,6 +43,14 @@ async function run() {
       res.send(result)
     })
 
+    // specific toy by id
+    app.get('/alltoys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addToyCollection.findOne(query);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -56,9 +64,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('toy hut server is running')
+  res.send('toy hut server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Toy hut server is running on port: ${port}`);
+  console.log(`Toy hut server is running on port: ${port}`);
 })
