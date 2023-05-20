1fe27ecd69aa6144f1ddf9a1a81ec2349bb6a185
siraz015 <siraj015213@gmail.com>
2023-05-20 13:17:24 +0600

    create updated api

--- index.js
+++ index.js
@@ -62,6 +62,19 @@ async function run() {
       res.send(result)
     })
 
+    // create api for delete operation
+    app.delete('/mytoys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addToyCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // create api for Update Operation
+    app.put('/mytoys/:id', async (req, res) => {
+      const updatedToys = req.body;
+      
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
