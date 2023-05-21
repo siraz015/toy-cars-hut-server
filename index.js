3c5240045146b56d044630c96bf0e68f43e2cf03
siraz015 <siraj015213@gmail.com>
2023-05-21 15:52:53 +0600

    api tab details

--- index.js
+++ index.js
@@ -115,6 +115,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/toy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addToyCollection.findOne(query);
+      res.send(result);
+    })
+
 
 
     // Implement Search Option
